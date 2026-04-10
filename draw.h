03df7d6e2d2eb325bf39472a2281a676f3ece2c8
George Spanos <gespa11019@gmail.com>
2026-04-10 12:24:09 +0300

    add import guards

--- draw.h
+++ draw.h
@@ -1,4 +1,7 @@
+#ifndef DRAW_H
+#define DRAW_H
 void draw_pixel(int x, int y, unsigned int color);
 void draw_char(int x, int y, char c, unsigned int color);
 void draw_string(int x, int y, const char *s, unsigned int color);
 void draw_rect(int x, int y, int width, int height, unsigned int color);
+#endif
--- framebuffer.h
+++ framebuffer.h
@@ -1,2 +1,5 @@
+#ifndef FRAMEBUFFER_H
+#define FRAMEBUFFER_H
 extern unsigned int *framebuffer;
 void framebuffer_init();
+#endif
